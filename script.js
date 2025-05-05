637d509780d67ee535883baddce61cd7001d8117
tkseniyya <tkseniya02@mail.ru>
2025-05-05 19:39:26 +0300

    работа формы для отправки

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("burger").addEventListener("click", function() {
-        let a = document.querySelector(".aperture__header").classList.toggle("open")
-        console.log('asd' + a)
+        document.querySelector(".aperture__header").classList.toggle("open")
     })
 })
+async function sendData(data) {
+    return await fetch('/api/apply/', {
+      method: 'POST',
+      body: data,
+    })
+  }  
+function serializeForm(formNode) {
+    return new FormData(formNode)
+}
+async function handleFormSubmit(event) {
+    event.preventDefault()
+    const data = serializeForm(event.target)
+    const response = await sendData(data)
+}  
+const applicantForm = document.getElementById('email')
+applicantForm.addEventListener('submit', handleFormSubmit)
+
